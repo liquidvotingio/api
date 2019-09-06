7d5b6109cd567a6ed534cddf648c7eadbe60db9d
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2019-09-06 18:34:20 +0200

    Add Participant with just a name, in Voting context

--- lib/liquid_dem/voting.ex
+++ lib/liquid_dem/voting.ex
@@ -101,4 +101,100 @@ defmodule LiquidDem.Voting do
   def change_proposal(%Proposal{} = proposal) do
     Proposal.changeset(proposal, %{})
   end
+
+  alias LiquidDem.Voting.Participant
+
+  @doc """
+  Returns the list of participants.
+
+  ## Examples
+
+      iex> list_participants()
+      [%Participant{}, ...]
+
+  """
+  def list_participants do
+    Repo.all(Participant)
+  end
+
+  @doc """
+  Gets a single participant.
+
+  Raises `Ecto.NoResultsError` if the Participant does not exist.
+
+  ## Examples
+
+      iex> get_participant!(123)
+      %Participant{}
+
+      iex> get_participant!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_participant!(id), do: Repo.get!(Participant, id)
+
+  @doc """
+  Creates a participant.
+
+  ## Examples
+
+      iex> create_participant(%{field: value})
+      {:ok, %Participant{}}
+
+      iex> create_participant(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_participant(attrs \\ %{}) do
+    %Participant{}
+    |> Participant.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a participant.
+
+  ## Examples
+
+      iex> update_participant(participant, %{field: new_value})
+      {:ok, %Participant{}}
+
+      iex> update_participant(participant, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_participant(%Participant{} = participant, attrs) do
+    participant
+    |> Participant.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a Participant.
+
+  ## Examples
+
+      iex> delete_participant(participant)
+      {:ok, %Participant{}}
+
+      iex> delete_participant(participant)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_participant(%Participant{} = participant) do
+    Repo.delete(participant)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking participant changes.
+
+  ## Examples
+
+      iex> change_participant(participant)
+      %Ecto.Changeset{source: %Participant{}}
+
+  """
+  def change_participant(%Participant{} = participant) do
+    Participant.changeset(participant, %{})
+  end
 end
--- test/liquid_dem/voting_test.exs
+++ test/liquid_dem/voting_test.exs
@@ -61,4 +61,63 @@ defmodule LiquidDem.VotingTest do
       assert %Ecto.Changeset{} = Voting.change_proposal(proposal)
     end
   end
+
+  describe "participants" do
+    alias LiquidDem.Voting.Participant
+
+    @valid_attrs %{name: "some name"}
+    @update_attrs %{name: "some updated name"}
+    @invalid_attrs %{name: nil}
+
+    def participant_fixture(attrs \\ %{}) do
+      {:ok, participant} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Voting.create_participant()
+
+      participant
+    end
+
+    test "list_participants/0 returns all participants" do
+      participant = participant_fixture()
+      assert Voting.list_participants() == [participant]
+    end
+
+    test "get_participant!/1 returns the participant with given id" do
+      participant = participant_fixture()
+      assert Voting.get_participant!(participant.id) == participant
+    end
+
+    test "create_participant/1 with valid data creates a participant" do
+      assert {:ok, %Participant{} = participant} = Voting.create_participant(@valid_attrs)
+      assert participant.name == "some name"
+    end
+
+    test "create_participant/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Voting.create_participant(@invalid_attrs)
+    end
+
+    test "update_participant/2 with valid data updates the participant" do
+      participant = participant_fixture()
+      assert {:ok, %Participant{} = participant} = Voting.update_participant(participant, @update_attrs)
+      assert participant.name == "some updated name"
+    end
+
+    test "update_participant/2 with invalid data returns error changeset" do
+      participant = participant_fixture()
+      assert {:error, %Ecto.Changeset{}} = Voting.update_participant(participant, @invalid_attrs)
+      assert participant == Voting.get_participant!(participant.id)
+    end
+
+    test "delete_participant/1 deletes the participant" do
+      participant = participant_fixture()
+      assert {:ok, %Participant{}} = Voting.delete_participant(participant)
+      assert_raise Ecto.NoResultsError, fn -> Voting.get_participant!(participant.id) end
+    end
+
+    test "change_participant/1 returns a participant changeset" do
+      participant = participant_fixture()
+      assert %Ecto.Changeset{} = Voting.change_participant(participant)
+    end
+  end
 end
