c4e4e92216efe9f77ed0767bca1dff81ded61d50
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2019-09-14 16:50:40 +0200

    Update instructions to run dockerized version, add defaults for some env vars

--- config/prod.exs
+++ config/prod.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 config :liquid_voting, LiquidVotingWeb.Endpoint,
   url: [
-    host: System.fetch_env!("APP_HOSTNAME"),
-    port: String.to_integer(System.fetch_env!("APP_PORT"))
+    host: System.get_env("APP_HOSTNAME") || "localhost" ,
+    port: String.to_integer(System.get_env("APP_PORT") || "4000")
   ],
   server: true
 
