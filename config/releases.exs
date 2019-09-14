b35f20c38a2ce4ffb003e7c15de4a10396049559
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2019-09-14 14:52:42 +0200

    Configure environment variables as part of release building

--- config/releases.exs
+++ config/releases.exs
@@ -1,17 +1,17 @@
 import Config
 
-# ecto://USER:PASS@HOST/DATABASE
-database_url = System.fetch_env!("DATABASE_URL") 
+database_url = System.fetch_env!("DATABASE_URL")
+pool_size = System.fetch_env!("DB_POOL_SIZE")
+port = System.fetch_env!("APP_PORT")
+secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
 
 config :liquid_voting, LiquidVoting.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
+  pool_size: String.to_integer(pool_size)
 
 config :liquid_voting, LiquidVotingWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [:inet6, port: String.to_integer(port)],
   secret_key_base: secret_key_base
 
 # to test the release locally:
