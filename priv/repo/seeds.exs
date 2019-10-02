72aab544789a5aff1cd976270b84bb13630ecb53
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2019-10-02 18:44:04 +0200

    Comment seeds out, so one has to use the API from the get go

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,41 +1,41 @@
-alias LiquidVoting.Repo
-alias LiquidVoting.Voting
-alias LiquidVoting.VotingResults
+# alias LiquidVoting.Repo
+# alias LiquidVoting.Voting
+# alias LiquidVoting.VotingResults
 
-proposal = Voting.create_proposal!(%{url: "some.proposal.on.github.com"})
+# proposal = Voting.create_proposal!(%{url: "some.proposal.on.github.com"})
 
-participant = Voting.create_participant!(%{name: "Lucia Coelho"})
-delegator = Voting.create_participant!(%{name: "Zubin Kurozawa"})
-delegator2 = Voting.create_participant!(%{name: "Louie Louie"})
+# participant = Voting.create_participant!(%{name: "Lucia Coelho"})
+# delegator = Voting.create_participant!(%{name: "Zubin Kurozawa"})
+# delegator2 = Voting.create_participant!(%{name: "Louie Louie"})
 
-Voting.create_delegation!(%{
-  delegator_id: delegator.id,
-  delegate_id: participant.id
-})
+# Voting.create_delegation!(%{
+#   delegator_id: delegator.id,
+#   delegate_id: participant.id
+# })
 
-Voting.create_delegation!(%{
-  delegator_id: delegator2.id,
-  delegate_id: participant.id
-})
+# Voting.create_delegation!(%{
+#   delegator_id: delegator2.id,
+#   delegate_id: participant.id
+# })
 
-participant = Repo.preload(participant, :delegations_received)
+# participant = Repo.preload(participant, :delegations_received)
 
-vote =
-  Voting.create_vote!(%{
-    yes: true,
-    proposal_id: proposal.id,
-    participant_id: participant.id
-  })
+# vote =
+#   Voting.create_vote!(%{
+#     yes: true,
+#     proposal_id: proposal.id,
+#     participant_id: participant.id
+#   })
 
-participant2 = Voting.create_participant!(%{name: "Francine Dunlop"})
+# participant2 = Voting.create_participant!(%{name: "Francine Dunlop"})
 
-vote =
-  Voting.create_vote!(%{
-    yes: false,
-    proposal_id: proposal.id,
-    participant_id: participant2.id
-  })
+# vote =
+#   Voting.create_vote!(%{
+#     yes: false,
+#     proposal_id: proposal.id,
+#     participant_id: participant2.id
+#   })
 
-VotingResults.calculate_result!(proposal)
-|> IO.inspect
+# VotingResults.calculate_result!(proposal)
+# |> IO.inspect
 
