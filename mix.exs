6c5cd3d27562a1a0ce379b89c164e41334f243b5
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2019-10-05 18:28:43 +0200

    Don't recreate the database each time a test is run. Do create it on CI

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule LiquidVoting.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.migrate", "test"]
     ]
   end
 end
