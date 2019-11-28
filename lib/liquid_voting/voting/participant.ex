251dd13e856475fa34817db085b44fc9e4e55d5e
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2019-11-28 17:48:31 +0100

    Use EctoFields.Email type on participant email for validation

--- lib/liquid_voting/voting/participant.ex
+++ lib/liquid_voting/voting/participant.ex
@@ -4,11 +4,9 @@ defmodule LiquidVoting.Voting.Participant do
 
   alias LiquidVoting.Voting.{Vote, Delegation}
 
-  @email_format ~r/^[A-Za-z0-9\._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$/
-
   schema "participants" do
     field :name, :string
-    field :email, :string
+    field :email, EctoFields.Email
 
     has_many :votes, Vote
     has_many :delegations_received, Delegation, foreign_key: :delegate_id
@@ -21,7 +19,6 @@ defmodule LiquidVoting.Voting.Participant do
     participant
     |> cast(attrs, [:name, :email])
     |> validate_required(:email)
-    |> validate_format(:email, @email_format)
     |> unique_constraint(:email)
   end
 end
