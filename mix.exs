576013f5bda2e5920b4408ae6250ae6418877bfa
Dave Frey <dave.frey@thetrainline.com>
2020-04-07 17:26:16 +0200

    Freeze postgrex version spec to 0.15.0

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule LiquidVoting.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, "~> 0.15.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
