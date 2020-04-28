d792c50b3b304152d53d5bb7b7620b40c3afbc07
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-04-28 18:16:21 +0200

    Fix voting weight and support both delegations that are specific to a proposal and global ones
    Before this all delegations were being treated as global, even if they had a proposal_url set

--- lib/liquid_voting/voting_weight.ex
+++ lib/liquid_voting/voting_weight.ex
@@ -25,7 +25,7 @@ defmodule LiquidVoting.VotingWeight do
     vote = Repo.preload(vote, [participant: :delegations_received], force: true)    
     voter = vote.participant
     
-    weight = 1 + delegation_weight(voter.delegations_received)
+    weight = 1 + delegation_weight(voter.delegations_received, vote.proposal_url)
 
     Voting.update_vote(vote, %{weight: weight})
   end
@@ -33,12 +33,12 @@ defmodule LiquidVoting.VotingWeight do
   # If no weight is given, adds default of 0 for it, and recurses with
   # delegation_weight/2. This is the starting point for traversing
   # a voter's delegation tree and incrementing the voting weight
-  defp delegation_weight(delegations, weight \\ 0)
+  defp delegation_weight(delegations, proposal_url, weight \\ 0)
 
   # Traverse up delegation tree and add up the accumulated weight.
   # If you're wondering, [_|_] matches a non-empty array. Equivalent to 
   # matching [head|tail] but ignoring both head and tail variables
-  defp delegation_weight(delegations = [_|_], weight) do
+  defp delegation_weight(delegations = [_|_], proposal_url, weight) do
     # TODO: Do this in SQL
     #
     #   Not sure yet which tree/hierarchy handling pattern would fit here, 
@@ -52,19 +52,27 @@ defmodule LiquidVoting.VotingWeight do
     # Add-up 1 unit of weight for each delegation,
     # then recurse on each delegators' own delegations
     Enum.reduce delegations, weight, fn (delegation, weight) ->
-      delegation = Repo.preload(delegation, [delegator: :delegations_received])
-      delegator = delegation.delegator
+      # Only proceed if delegation is global or is meant for the
+      # proposal being voted on:
+      if delegation.proposal_url == proposal_url || delegation.proposal_url == nil do
+        delegation = Repo.preload(delegation, [delegator: :delegations_received])
+        delegator = delegation.delegator
 
-      weight = weight + 1
+        weight = weight + 1
 
-      delegation_weight(delegator.delegations_received, weight)
+        delegation_weight(delegator.delegations_received, proposal_url, weight)
+
+      # If delegation is for a different proposal, just return the unchanged weight
+      else
+        weight
+      end
     end
   end
 
   # Base case for the above recursion:
   # 
   # If no delegations left, just return the latest weight
-  defp delegation_weight(_ = [], weight) do
+  defp delegation_weight(_ = [], _, weight) do
     weight
   end
 end
