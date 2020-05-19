df1e64aff6ebe143bfa3ab3cd4c9001d56e9f874
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-05-19 17:32:26 +0200

    Add organization_uuid to factories
    Part of #31

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -7,7 +7,8 @@ defmodule LiquidVoting.Factory do
   def participant_factory do
     %Participant{
       name: sequence(:name, &"Jane Doe #{&1}"),
-      email: sequence(:email, &"jane#{&1}@somedomain.com")
+      email: sequence(:email, &"jane#{&1}@somedomain.com"),
+      organization_uuid: Ecto.UUID.generate
     }
   end
 
@@ -15,7 +16,8 @@ defmodule LiquidVoting.Factory do
     %Vote{
       yes: true,
       proposal_url: sequence(:proposal_url, &"https://proposals.com/#{&1}"),
-      participant: build(:participant)
+      participant: build(:participant),
+      organization_uuid: Ecto.UUID.generate
     }
   end
 
@@ -23,10 +25,16 @@ defmodule LiquidVoting.Factory do
     %Delegation{
       delegator: build(:participant),
       delegate: build(:participant),
+      organization_uuid: Ecto.UUID.generate
     }
   end
 
   def voting_result_factory do
-    %Result{no: 0, yes: 0, proposal_url: sequence(:proposal_url, &"https://proposals.com/#{&1}")}
+    %Result{
+      no: 0,
+      yes: 0,
+      proposal_url: sequence(:proposal_url, &"https://proposals.com/#{&1}"),
+      organization_uuid: Ecto.UUID.generate
+    }
   end
 end
