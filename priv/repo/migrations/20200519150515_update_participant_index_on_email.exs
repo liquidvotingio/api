d42787bc247184ff3dc66ff012f706e0231f8a1f
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-05-19 17:09:41 +0200

    Add unique index and constraint on participant org uuid and email
    Part of #31

---
+++ priv/repo/migrations/20200519150515_update_participant_index_on_email.exs
@@ -0,0 +1,8 @@
+defmodule LiquidVoting.Repo.Migrations.UpdateParticipantIndexOnEmail do
+  use Ecto.Migration
+
+  def change do
+    drop unique_index(:participants, [:email])
+    create unique_index(:participants, [:organization_uuid, :email], name: :uniq_index_organization_uuid_participant_email)
+  end
+end
