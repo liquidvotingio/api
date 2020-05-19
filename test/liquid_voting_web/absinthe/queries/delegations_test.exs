47f9176e93b7849d0ddefab4a763aa50734778a1
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-05-19 18:26:06 +0200

    Include organization_uuid in tests for delegation mutations and queries
    Part of #31

--- test/liquid_voting_web/absinthe/queries/delegations_test.exs
+++ test/liquid_voting_web/absinthe/queries/delegations_test.exs
@@ -26,7 +26,7 @@ defmodule LiquidVotingWeb.Absinthe.Queries.DelegationsTest do
       }
       """
 
-      {:ok, %{data: %{"delegations" => [payload | _]}}} = Absinthe.run(query, Schema, context: %{})
+      {:ok, %{data: %{"delegations" => [payload | _]}}} = Absinthe.run(query, Schema, context: %{organization_uuid: Ecto.UUID.generate})
 
       assert payload["delegate"]["email"] == delegation.delegate.email
       assert payload["delegator"]["email"] == delegation.delegator.email
