217ce5476220981905eeb833c90252f9da56b6af
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-05-20 14:07:08 +0200

    Fix voting results. Part of #31

--- lib/liquid_voting/voting_results.ex
+++ lib/liquid_voting/voting_results.ex
@@ -11,21 +11,22 @@ defmodule LiquidVoting.VotingResults do
 
   @doc """
   Creates or updates voting result based on votes
-  given to a proposal
+  given to a proposal within the scope of a organization_uuid
 
   ## Examples
 
-      iex> calculate_result!("https://www.medium/user/eloquent_proposal")
+      iex> calculate_result!("https://www.medium/user/eloquent_proposal", "a6158b19-6bf6-4457-9d13-ef8b141611b4")
       %Result{}
 
   """
-  def calculate_result!(proposal_url) do
-    votes = Voting.list_votes(proposal_url)
+  def calculate_result!(proposal_url, organization_uuid) do
+    votes = Voting.list_votes(proposal_url, organization_uuid)
 
     attrs = %{
       yes: 0,
       no: 0,
-      proposal_url: proposal_url
+      proposal_url: proposal_url,
+      organization_uuid: organization_uuid
     }
 
     attrs =
@@ -43,12 +44,12 @@ defmodule LiquidVoting.VotingResults do
     |> Result.changeset(attrs)
     |> Repo.insert!(
       on_conflict: :replace_all_except_primary_key,
-      conflict_target: [:proposal_url]
+      conflict_target: [:organization_uuid, :proposal_url]
       )
   end
 
-  def publish_voting_result_change(proposal_url) do
-    result = calculate_result!(proposal_url)
+  def publish_voting_result_change(proposal_url, organization_uuid) do
+    result = calculate_result!(proposal_url, organization_uuid)
 
     Absinthe.Subscription.publish(
       LiquidVotingWeb.Endpoint,
