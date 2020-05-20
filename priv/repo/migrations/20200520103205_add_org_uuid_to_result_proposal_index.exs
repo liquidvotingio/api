80be21fa811824c36451570d04b8cadeea856828
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-05-20 13:48:35 +0200

    Update voting results indexing and contraint
    Unique index and constraint on organization_uuid and proposal_url. Part of #31

---
+++ priv/repo/migrations/20200520103205_add_org_uuid_to_result_proposal_index.exs
@@ -0,0 +1,8 @@
+defmodule LiquidVoting.Repo.Migrations.AddOrgUuidToResultProposalIndex do
+  use Ecto.Migration
+
+  def change do
+    drop unique_index(:results, [:proposal_url])
+    create unique_index(:results, [:organization_uuid, :proposal_url], name: :uniq_index_organization_uuid_proposal_url)
+  end
+end
