ad552849df0b3134c4a919bec832281fb82c38b5
nkls <niklas@long.ch>
2020-06-29 10:40:40 +0200

    Refactor Resolvers.VotingResults

--- lib/liquid_voting_web/resolvers/voting_results.ex
+++ lib/liquid_voting_web/resolvers/voting_results.ex
@@ -1,7 +1,6 @@
 defmodule LiquidVotingWeb.Resolvers.VotingResults do
   alias LiquidVoting.VotingResults
 
-  def result(_, %{proposal_url: proposal_url}, %{context: %{organization_uuid: organization_uuid}}) do
-    {:ok, VotingResults.get_result_by_proposal_url(proposal_url, organization_uuid)}
-  end
+  def result(_, %{proposal_url: proposal_url}, %{context: %{organization_uuid: organization_uuid}}),
+      do: {:ok, VotingResults.get_result_by_proposal_url(proposal_url, organization_uuid)}
 end
