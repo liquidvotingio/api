ed1ea9a5d1f126732d8c3c7defc8f6e908d3da55
nkls <niklas@long.ch>
2020-06-29 10:40:06 +0200

    Refactor LiquidVotingWeb

--- lib/liquid_voting_web.ex
+++ lib/liquid_voting_web.ex
@@ -23,6 +23,7 @@ defmodule LiquidVotingWeb do
 
       import Plug.Conn
       import LiquidVotingWeb.Gettext
+
       alias LiquidVotingWeb.Router.Helpers, as: Routes
     end
   end
@@ -35,9 +36,9 @@ defmodule LiquidVotingWeb do
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
-
       import LiquidVotingWeb.ErrorHelpers
       import LiquidVotingWeb.Gettext
+
       alias LiquidVotingWeb.Router.Helpers, as: Routes
     end
   end
@@ -45,6 +46,7 @@ defmodule LiquidVotingWeb do
   def router do
     quote do
       use Phoenix.Router
+
       import Plug.Conn
       import Phoenix.Controller
     end
@@ -60,7 +62,5 @@ defmodule LiquidVotingWeb do
   @doc """
   When used, dispatch to the appropriate controller/view/etc.
   """
-  defmacro __using__(which) when is_atom(which) do
-    apply(__MODULE__, which, [])
-  end
+  defmacro __using__(which) when is_atom(which), do: apply(__MODULE__, which, [])
 end
