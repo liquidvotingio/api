c2861b9b57b7f4abb822ed816dd9662b0d84fa91
nkls <niklas@long.ch>
2020-06-29 10:41:28 +0200

    Refactor ErrorView

--- lib/liquid_voting_web/views/error_view.ex
+++ lib/liquid_voting_web/views/error_view.ex
@@ -10,7 +10,6 @@ defmodule LiquidVotingWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
-  def template_not_found(template, _assigns) do
-    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
-  end
+  def template_not_found(template, _assigns),
+    do: %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
 end
