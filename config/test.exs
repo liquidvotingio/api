8a6d330f456e2f96b6b1773de1123bb498d84c01
Simon Tharby <jinjamail@gmail.com>
2020-07-15 17:35:41 +0100

    move migration p_key config to config.exs

--- config/test.exs
+++ config/test.exs
@@ -5,8 +5,7 @@ config :liquid_voting, LiquidVoting.Repo,
   password: "postgres",
   database: "liquid_voting_test",
   hostname: System.get_env("POSTGRES_HOST") || "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  migration_primary_key: [name: :id, type: :binary_id]
+  pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
