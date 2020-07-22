d290115d673203eaa3bf16dcd6ba1455476bde6c
nkls <niklas@long.ch>
2020-07-22 11:47:33 +0200

    Fix Phoenix.ConnTest deprecation warnings

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -18,7 +18,9 @@ defmodule LiquidVotingWeb.ConnCase do
   using do
     quote do
       # Import conveniences for testing with connections
-      use Phoenix.ConnTest
+      import Plug.Conn
+      import Phoenix.ConnTest
+
       alias LiquidVotingWeb.Router.Helpers, as: Routes
 
       # The default endpoint for testing
