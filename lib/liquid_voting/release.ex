e0337b1adc4096954f1d76c29e76451c0202140b
Simon Tharby <jinjamail@gmail.com>
2020-07-24 13:18:34 +0100

    add teardown/0 to release.ex

--- lib/liquid_voting/release.ex
+++ lib/liquid_voting/release.ex
@@ -1,6 +1,8 @@
 defmodule LiquidVoting.Release do
   @app :liquid_voting
 
+  alias LiquidVoting.{Delegations, Voting}
+
   def migrate do
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
@@ -9,6 +11,23 @@ defmodule LiquidVoting.Release do
 
   def rollback(repo, version) do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  @doc """
+  Deletes votes, participants and delegations data created by smoke tests.
+  For use before and after running smoke tests.
+  """
+  def teardown() do
+    organization_id = "bc7eeccb-5e10-4004-8bfb-7fc68536bbd7"
+
+    votes = Voting.list_votes(organization_id)
+    Enum.each(votes, fn vote -> Voting.delete_vote!(vote) end)
+
+    participants = Voting.list_participants(organization_id)
+    Enum.each(participants, fn participant -> Voting.delete_participant(participant) end)
+
+    delegations = Delegations.list_delegations(organization_id)
+    Enum.each(delegations, fn delegation -> Delegations.delete_delegation!(delegation) end)
   end
 
   defp repos do
