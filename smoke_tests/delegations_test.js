2055276bd52cd215721ffd708d1724ae14326367
Simon Tharby <jinjamail@gmail.com>
2020-07-29 15:04:06 +0100

    try to read correct __ENV.

--- smoke_tests/delegations_test.js
+++ smoke_tests/delegations_test.js
@@ -20,7 +20,7 @@ const HEADERS = {
 
 export default () => {
   group("Global delegations", () => {
-    var r = `${__ENV.API_AUTH_KEYXX}`;
+    var r = `${__ENV.TEST_API_AUTH_KEY}`;
     console.log(`auth key env var: ${r}`);
 
 
