e66d8b363310c02c0f4ec9880d275aff5946989c
Simon Tharby <jinjamail@gmail.com>
2020-08-04 20:24:36 +0100

    test of conditional fail using grep

--- grepit.sh
+++ grepit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-msg=$(mix sobelow)
+msg=$(mix sobelow --config --verbose)
 echo "$msg"
 
-error=$(echo "$msg" | grep -o 'Confidence')
+error=$(echo "$msg" | grep -o 'Confidence' | wc -l)
 
 echo
 
