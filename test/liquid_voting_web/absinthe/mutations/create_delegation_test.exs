d84f41eb359cfad68d82b4f0f0fdefa9a5f138a2
Simon Tharby <jinjamail@gmail.com>
2020-09-01 13:51:02 +0100

    add test of identical ids + switch test of identical email to using context (for consistency)

--- test/liquid_voting_web/absinthe/mutations/create_delegation_test.exs
+++ test/liquid_voting_web/absinthe/mutations/create_delegation_test.exs
@@ -191,11 +191,34 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegationTest do
       assert details == %{delegate_id: ["can't be blank"]}
     end
 
-    test "with identical emails for delegator and delegate" do
+    test "with identical emails for delegator and delegate", context do
       query = """
       mutation {
-        createDelegation(delegatorEmail: "#{@delegator_email}", delegateEmail: "#{
-        @delegator_email
+        createDelegation(delegatorEmail: "#{context[:delegator].email}", delegateEmail: "#{
+        context[:delegator].email
+      }") {
+          delegator {
+            email
+          }
+          delegate {
+            email
+          }
+        }
+      }
+      """
+
+      {:ok, %{errors: [%{message: message, details: details}]}} =
+        Absinthe.run(query, Schema, context: %{organization_id: Ecto.UUID.generate()})
+
+      assert message == "Could not create delegation"
+      assert details == %{delegate_id: ["delegator and delegate must be different"]}
+    end
+
+    test "with identical ids for delegator and delegate", context do
+      query = """
+      mutation {
+        createDelegation(delegatorId: "#{context[:delegator].id}", delegateId: "#{
+        context[:delegator].id
       }") {
           delegator {
             email
