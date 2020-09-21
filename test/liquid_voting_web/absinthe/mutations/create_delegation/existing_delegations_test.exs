399314682bdcfd74f5c76b514e7b1834d436c1a6
Simon Tharby <jinjamail@gmail.com>
2020-09-21 15:21:02 +0100

    two (failing) absinthe tests - 1 of each case

--- test/liquid_voting_web/absinthe/mutations/create_delegation/existing_delegations_test.exs
+++ test/liquid_voting_web/absinthe/mutations/create_delegation/existing_delegations_test.exs
@@ -7,9 +7,10 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.ExistingDelegation
   @delegate_email "delegate@email.com"
   @another_delegate_email "another-delegate@email.com"
   @proposal_url "https://www.proposal.com/my"
+  @another_proposal_url "https://www.proposal.com/another"
   @organization_id Ecto.UUID.generate()
 
-  describe "with existing global delegation" do
+  describe "create global delegation for delegator with existing global delegation to different delegate" do
     test "overwrites existing global delegation" do
       query = """
       mutation {
@@ -54,6 +55,98 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.ExistingDelegation
 
       assert modified_delegation["delegator"]["email"] == @delegator_email
       assert modified_delegation["delegate"]["email"] == @another_delegate_email
+    end
+  end
+
+  describe "create global delegation for delegator with existing proposal delegations to same delegate" do
+    test "deletes proposal specific delegations for same delegator/delegate pair" do
+      # first, create a proposal-specific delegation
+      query = """
+      mutation {
+        createDelegation(delegatorEmail: "#{@delegator_email}", delegateEmail: "#{@delegate_email}", proposalUrl: "#{
+        @proposal_url
+      }") {
+          id
+          proposalUrl
+        }
+      }
+      """
+
+      {:ok, %{data: %{"createDelegation" => proposal_delegation_1}}} =
+        Absinthe.run(query, Schema, context: %{organization_id: @organization_id})
+
+      assert proposal_delegation_1["proposalUrl"] == @proposal_url
+
+      # second, create another proposal-specific delegation (same participants, different proposalUrl)
+      query = """
+      mutation {
+        createDelegation(delegatorEmail: "#{@delegator_email}", delegateEmail: "#{@delegate_email}", proposalUrl: "#{
+        @another_proposal_url
+      }") {
+          id
+          proposalUrl
+        }
+      }
+      """
+
+      {:ok, %{data: %{"createDelegation" => proposal_delegation_2}}} =
+        Absinthe.run(query, Schema, context: %{organization_id: @organization_id})
+
+      assert proposal_delegation_2["proposalUrl"] == @another_proposal_url
+
+      # TODO? Insert 3rd proposal-specific delegation to DIFFERENT delegate, for testing deletion of wrong delegations does not occur?
+
+      # third, create a global delegation for the same participants
+      query = """
+      mutation {
+        createDelegation(delegatorEmail: "#{@delegator_email}", delegateEmail: "#{@delegate_email}") {
+          delegator {
+            email
+          }
+          delegate {
+            email
+          }
+        }
+      }
+      """
+
+      {:ok, %{data: %{"createDelegation" => global_delegation}}} =
+        Absinthe.run(query, Schema, context: %{organization_id: @organization_id})
+
+      assert global_delegation["delegator"]["email"] == @delegator_email
+      assert global_delegation["delegate"]["email"] == @delegate_email
+
+      # fourth, search for proposal_delgation_1 (should return error)
+      query = """
+      query {
+        delegation(id: "#{proposal_delegation_1["id"]}") {
+          id
+        }
+      }
+      """
+
+      # TODO: Change to {:ok, %{errors: ...}} and assert specific error, when functionality created
+      {:ok, %{data: %{"delegation" => delegation}}} =
+        Absinthe.run(query, Schema, context: %{organization_id: @organization_id})
+
+      assert delegation["id"] != proposal_delegation_1["id"]
+
+      # lastly, search for proposal_delegation_2 (should return error)
+      query = """
+      query {
+        delegation(id: "#{proposal_delegation_2["id"]}") {
+          id
+        }
+      }
+      """
+
+      # TODO: Change to {:ok, %{errors: ...}} and assert specific error, when functionality created
+      {:ok, %{data: %{"delegation" => delegation}}} =
+        Absinthe.run(query, Schema, context: %{organization_id: @organization_id})
+
+      assert delegation["id"] != proposal_delegation_2["id"]
+
+      # TODO? Search for 3rd proposal-specific delegation to DIFFERENT delegate, and assert can be found (not deleted)?
     end
   end
 
@@ -103,6 +196,54 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.ExistingDelegation
       assert modified_delegation["delegator"]["email"] == @delegator_email
       assert modified_delegation["delegate"]["email"] == @another_delegate_email
       assert original_delegation["proposalUrl"] == modified_delegation["proposalUrl"]
+    end
+  end
+
+  describe "create proposal delegation for delegator with existing global delegation to same delegate" do
+    test "returns error" do
+      # first, create a global delegation
+      query = """
+      mutation {
+        createDelegation(delegatorEmail: "#{@delegator_email}", delegateEmail: "#{@delegate_email}") {
+          delegator {
+            email
+          }
+          delegate {
+            email
+          }
+        }
+      }
+      """
+
+      {:ok, %{data: %{"createDelegation" => original_delegation}}} =
+        Absinthe.run(query, Schema, context: %{organization_id: @organization_id})
+
+      assert original_delegation["delegator"]["email"] == @delegator_email
+      assert original_delegation["delegate"]["email"] == @delegate_email
+
+      # then, attempt to create a proposal-specific delegation for same delegator and delegate
+      query = """
+      mutation {
+        createDelegation(delegatorEmail: "#{@delegator_email}", delegateEmail: "#{@delegate_email}", proposalUrl: "#{
+        @proposal_url
+      }") {
+          delegator {
+            email
+          }
+          delegate {
+            email
+          }
+          proposalUrl
+        }
+      }
+      """
+
+      # TODO: Change to {:ok, %{errors: ...}} and assert specific error, when functionality created
+      {:ok, %{data: %{"createDelegation" => new_delegation}}} =
+        Absinthe.run(query, Schema, context: %{organization_id: @organization_id})
+
+      assert new_delegation["delegator"]["email"] != @delegator_email
+      assert new_delegation["delegate"]["email"] != @delegate_email
     end
   end
 end
