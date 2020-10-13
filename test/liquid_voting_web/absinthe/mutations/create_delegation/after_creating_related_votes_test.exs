943d477bfdae36fce25f8a32b547e4b56b12e801
Simon Tharby <jinjamail@gmail.com>
2020-10-13 13:35:46 +0100

    Add coments and rename vars

--- test/liquid_voting_web/absinthe/mutations/create_delegation/after_creating_related_votes_test.exs
+++ test/liquid_voting_web/absinthe/mutations/create_delegation/after_creating_related_votes_test.exs
@@ -8,7 +8,7 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
     test "after creating related proposal vote by delegate" do
       # Insert vote and get related participant and proposal_url.
       vote = insert(:vote, yes: true)
-      participant = vote.participant
+      voter = vote.participant
       proposal_url = vote.proposal_url
 
       # Insert participant to act as delegator.
@@ -18,9 +18,9 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
       # voting result for proposal_url.
       query = """
       mutation {
-        createDelegation(delegatorEmail: "#{delegator.email}", delegateEmail: "#{
-        participant.email
-      }", proposalUrl: "#{proposal_url}") {
+        createDelegation(delegatorEmail: "#{delegator.email}", delegateEmail: "#{voter.email}", proposalUrl: "#{
+        proposal_url
+      }") {
           proposalUrl
           votingResult {
             inFavor
@@ -33,6 +33,7 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
       {:ok, %{data: %{"createDelegation" => delegation}}} =
         Absinthe.run(query, Schema, context: %{organization_id: vote.organization_id})
 
+      # Assert 'in favor' vount count now == 2, due to the delegation given to the voter
       assert delegation["proposalUrl"] == proposal_url
       assert delegation["votingResult"]["inFavor"] == 2
       assert delegation["votingResult"]["against"] == 0
@@ -43,7 +44,7 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
     test "after creating vote by delegate" do
       # Insert vote and get related participant and proposal_url.
       vote = insert(:vote, yes: false)
-      participant = vote.participant
+      voter = vote.participant
       proposal_url = vote.proposal_url
 
       # Insert participant to act as delegator
@@ -57,9 +58,7 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
       # returned in the final related query. Hence, an absinthe mutation is used.
       query = """
       mutation {
-        createDelegation(delegatorEmail: "#{delegator.email}", delegateEmail: "#{
-        participant.email
-      }") {
+        createDelegation(delegatorEmail: "#{delegator.email}", delegateEmail: "#{voter.email}") {
           delegator {
             email
           }
@@ -74,7 +73,7 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
         Absinthe.run(query, Schema, context: %{organization_id: vote.organization_id})
 
       assert delegation["delegator"]["email"] == delegator.email
-      assert delegation["delegate"]["email"] == participant.email
+      assert delegation["delegate"]["email"] == voter.email
 
       # Get result for proposal_B_url (should be 'against == 2').
       query = """
@@ -100,24 +99,24 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
     test "after creating related votes" do
       # Insert vote_A and get related participant and proposal_url.
       vote_A = insert(:vote, yes: true)
-      participant_A = vote_A.participant
+      voter_A = vote_A.participant
       proposal_A_url = vote_A.proposal_url
 
-      # Insert vote_B and get related participant asnd proposal_url.
+      # Insert vote_B and get related participant and proposal_url.
       vote_B = insert(:vote, yes: false, organization_id: vote_A.organization_id)
-      participant_B = vote_B.participant
+      voter_B = vote_B.participant
       proposal_B_url = vote_B.proposal_url
 
       # Insert participant to act as delegator.
       delegator = insert(:participant, organization_id: vote_A.organization_id)
 
-      # Create proposal_A_url delegation to participant_A (as delegate) and get
+      # Create proposal_A_url delegation to voter_A (as delegate) and get
       # voting result for proposal_A_url.
       query = """
       mutation {
-        createDelegation(delegatorEmail: "#{delegator.email}", delegateEmail: "#{
-        participant_A.email
-      }", proposalUrl: "#{proposal_A_url}") {
+        createDelegation(delegatorEmail: "#{delegator.email}", delegateEmail: "#{voter_A.email}", proposalUrl: "#{
+        proposal_A_url
+      }") {
           proposalUrl
           votingResult {
             inFavor
@@ -130,11 +129,12 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
       {:ok, %{data: %{"createDelegation" => delegation}}} =
         Absinthe.run(query, Schema, context: %{organization_id: vote_A.organization_id})
 
+      # Assert 'in favor' vote count == 2, due to delegation given to voter_A.
       assert delegation["proposalUrl"] == proposal_A_url
       assert delegation["votingResult"]["inFavor"] == 2
       assert delegation["votingResult"]["against"] == 0
 
-      # Create global delegation to participant_B (as delegate)
+      # Create global delegation to voter_B (as delegate).
       #
       # This setup step needs to involve a call to create_delegation, as simply
       # inserting a factory delegation cannot be expected to call
@@ -142,9 +142,7 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
       # returned in the final related query. Hence, an absinthe mutation is used.
       query = """
       mutation {
-        createDelegation(delegatorEmail: "#{delegator.email}", delegateEmail: "#{
-        participant_B.email
-      }") {
+        createDelegation(delegatorEmail: "#{delegator.email}", delegateEmail: "#{voter_B.email}") {
           delegator {
             email
           }
@@ -159,9 +157,9 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
         Absinthe.run(query, Schema, context: %{organization_id: vote_A.organization_id})
 
       assert delegation["delegator"]["email"] == delegator.email
-      assert delegation["delegate"]["email"] == participant_B.email
+      assert delegation["delegate"]["email"] == voter_B.email
 
-      # Get result for proposal_B_url (should be 'against == 2').
+      # Get result for proposal_B_url.
       query = """
       query {
         votingResult(proposalUrl: "#{proposal_B_url}") {
@@ -175,6 +173,7 @@ defmodule LiquidVotingWeb.Absinthe.Mutations.CreateDelegation.AfterCreatingRelat
       {:ok, %{data: %{"votingResult" => result}}} =
         Absinthe.run(query, Schema, context: %{organization_id: vote_A.organization_id})
 
+      # Assert 'against' vote count == 2, due to delegation given to voter_B.
       assert result["proposalUrl"] == proposal_B_url
       assert result["inFavor"] == 0
       assert result["against"] == 2
