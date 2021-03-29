78e3ae86cbb0e84a308708a7d9dd78217c22c53d
Simon Tharby <jinjamail@gmail.com>
2021-03-29 19:56:47 +0100

    Add voting_methods migration.
    Create table and index of organization_id + voting_method.
    Related model not yet defined.

---
+++ priv/repo/migrations/20210329184924_add_voting_methods_table.exs
@@ -0,0 +1,14 @@
+defmodule LiquidVoting.Repo.Migrations.AddVotingMethodsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:voting_methods) do
+      add :voting_method, :text, default: nil, null: false
+      add :organization_id, :uuid, null: false
+
+      timestamps()
+    end
+
+    create index(:voting_methods, [:organization_id, :voting_method])
+  end
+end
