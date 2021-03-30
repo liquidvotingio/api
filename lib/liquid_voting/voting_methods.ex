5d9db0c7eb851c5937c4018c25c9d5214c4521cc
Simon Tharby <jinjamail@gmail.com>
2021-03-30 15:00:56 +0100

    Use insert_or_update in upsert_voting_method.

--- lib/liquid_voting/voting_methods.ex
+++ lib/liquid_voting/voting_methods.ex
@@ -23,13 +23,13 @@ defmodule LiquidVoting.VotingMethods do
     attrs =
       if Map.get(attrs, :voting_method) == nil,
         # Workaround: Prevent creation of new record for matching records
-        # with voting_method: nil, by providing default string value.
+        # where voting_method: nil, by providing default string value.
         do: Map.put(attrs, :voting_method, "** no method specified **"),
         else: attrs
 
     %VotingMethod{}
     |> VotingMethod.changeset(attrs)
-    |> Repo.insert(
+    |> Repo.insert_or_update(
       on_conflict: {:replace_all_except, [:id]},
       conflict_target: [:organization_id, :voting_method],
       returning: true
